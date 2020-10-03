291418346c39b7196f5130a81a671df11bcec622
alphatule <66392548+alphatule@users.noreply.github.com>
2020-10-03 18:09:38 +0200

    UPDATED to v2 VORP CORE

--- server/server.lua
+++ server/server.lua
@@ -1,12 +1,15 @@
-VORP = exports.vorp_core:vorpAPI()
+local VorpCore = {}
+
+TriggerEvent("getCore",function(core)
+    VorpCore = core
+end)
 
 RegisterServerEvent('vorp_bountyhunting:AddSomeMoney')
 AddEventHandler('vorp_bountyhunting:AddSomeMoney', function()
     local _source = source
     local price = Config.Price
     local xp = Config.Xp
-    TriggerEvent("vorp:getCharacter",source,function(user)
-        VORP.addMoney(_source, 0, price)
-        VORP.addXp(_source, xp)
-    end)
+    local Character = VorpCore.getUser(_source).getUsedCharacter
+    Character.addCurrency(0, price)
+    Character.addXp(xp)
 end)
